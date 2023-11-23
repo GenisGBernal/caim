b2caf49748b7e0385db44bedc3e492bec65968fc
GenisGB <genisgutierrezbernal@gmail.com>
2023-11-24 00:57:47 +0100

    MRKmeansStep

--- session6mapreduce/MRKmeansStep.py
+++ session6mapreduce/MRKmeansStep.py
@@ -35,7 +35,31 @@ class MRKmeansStep(MRJob):
 
         The result should be always a value in the range [0,1]
         """
-        return 1
+        intersect = 0
+        union = 0
+        i = 0
+        j = 0
+        while i < len(prot) and j < len(doc):
+            union += 1
+            if (prot[i][0] == doc[j]):
+                intersect += 1
+                i += 1
+                j += 1
+            elif prot[i][0] < doc[j]:
+                i += 1
+            else:
+                j += 1
+
+        while i < len(prot):
+            i += 1
+            union += 1
+        
+        while j < len(doc):
+            j += 1
+            union += 1
+
+        return intersect / float(union)
+
 
     def configure_args(self):
         """
@@ -75,12 +99,17 @@ class MRKmeansStep(MRJob):
         doc, words = line.split(':')
         lwords = words.split()
 
-        #
-        # Compute map here
-        #
+        bestSimilarity = 0
+        prototype = "null"
+
+        for k,v in self.prototypes.items():
+            similarity = self.jaccard(v, lwords)
+            if (similarity > bestSimilarity):
+                bestSimilarity = similarity
+                prototype = k
 
         # Return pair key, value
-        yield None, None
+        yield prototype, (doc, lwords)
 
     def aggregate_prototype(self, key, values):
         """
@@ -100,7 +129,27 @@ class MRKmeansStep(MRJob):
         :return:
         """
 
-        yield None, None
+        nDocuments = 0
+        documentList = []
+        prototypeMap = {}
+
+        for document in values:
+            nDocuments += 1
+            documentList.append(document)
+            for word in document[1]:
+                if word in prototypeMap:
+                    prototypeMap[word] += 1
+                else:
+                    prototypeMap[word] = 1
+
+        finalPrototype = []
+        for word, freq in prototypeMap.items():
+            finalPrototype.append((word, freq/float(nDocuments)))
+        
+        orderedDocumentList = sorted(documentList)
+        orderedFinalPrototype = sorted(finalPrototype, key = lambda x: x[0])
+
+        yield key, (orderedDocumentList, orderedFinalPrototype)
 
     def steps(self):
         return [MRStep(mapper_init=self.load_data, mapper=self.assign_prototype,
