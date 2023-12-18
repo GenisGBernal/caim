0c92329ae979199b19668d1c7550e0a60b25332c
GenisGB <genisgutierrezbernal@gmail.com>
2023-12-18 17:53:48 +0100

    no va

--- session9recomenders/similarity.py
+++ session9recomenders/similarity.py
@@ -1,13 +1,32 @@
+import numpy as np
+
 
 def compute_similarity(vector1: list, vector2: list):
     # # @TODO: Implement the actual computation of similarity between vector1 and vector2.
     # The current implementation returns a placeholder value of 1. Update this function 
     # to perform the appropriate similarity calculation and return the result.
-    return 1
     
+    # Convert lists to NumPy arrays
+    array1 = np.array(vector1)
+    array2 = np.array(vector2)
+
+    
+    # Compute magnitudes
+    magnitude1 = np.linalg.norm(array1)
+    magnitude2 = np.linalg.norm(array2)
+    if magnitude1 == 0 or magnitude2 == 0:
+        return 0
+
+    # Compute dot product
+    dot_product = np.dot(array1, array2)
+
+    # Compute cosine similarity
+    similarity = dot_product / (magnitude1 * magnitude2)
+
+    return similarity
 
 if __name__ == "__main__":
     
-    vector_a, vector_b = [1, 2, 3, 4], [4, 3, 2, 1]
-    compute_similarity(vector_a, vector_b)
-    
+    vector_a, vector_b = [0, 1], [1, 0]
+    x = compute_similarity(vector_a, vector_b)
+    print(x)
--- session9recomenders/user-based_recommender.py
+++ session9recomenders/user-based_recommender.py
@@ -8,28 +8,59 @@ import utils as ut
 
 def generate_m(movies_idx, users, ratings):
     # Complete the datastructure for rating matrix 
-    # @TODO 
-    
-    m = None
-    
+
+    x = ratings.iloc[2]
+
+    #All matrix values with -1.0
+    m = {userId: {movieId: -1.0 for movieId in movies_idx} for userId in users}
+
+    # Set matrix values efficiently
+    for _, row in ratings.iterrows():
+        m[row['userId']][row['movieId']] = row['rating']
+
     return m 
+
+def get_avg(ratings):
+    rated_ratings = [rate for rate in ratings if rate != -1.0]
+
+    if rated_ratings:
+        return sum(rated_ratings) / len(rated_ratings)
+    else:
+        return None
 
 
 def user_based_recommender(target_user_idx, matrix):
-    target_user = matrix.iloc[target_user_idx]
+    # target_user = matrix.iloc[target_user_idx]
+    target_user = matrix[target_user_idx]
+    target_user_ratings = list(target_user.values())
     recommendations = []
     
     # Compute the similarity between  the target user and each other user in the matrix. 
     # We recomend to store the results in a dataframe (userId and Similarity)
-    # @TODO 
+    data = {'userId': [], 'Similarity': []}
+    for userId, ratings in matrix.items():
+        if userId != target_user_idx:
+            rated_by_both_users_v1 = [v1 for v1, v2 in zip(target_user_ratings, ratings.values()) if v1 != -1.0 and v2 != -1.0]
+            rated_by_both_users_v2 = [v2 for v1, v2 in zip(target_user_ratings, ratings.values()) if v1 != -1.0 and v2 != -1.0]
+            data['userId'].append(userId)
+            data['Similarity'].append(sim.compute_similarity(rated_by_both_users_v1, rated_by_both_users_v2))
 
-    
+    df = pd.DataFrame(data)
+
     # Determine the unseen movies by the target user. Those films are identfied 
     # since don't have any rating. 
-    # @TODO 
+    not_rated_movies = [movieId for movieId, rating in target_user.items() if rating == -1.0]
      
     # Generate recommendations for unrated movies based on user similarity and ratings.
     # @ TODO 
+    avg_rating_target = get_avg(target_user_ratings)
+    expected_rates = []
+    for movieId in not_rated_movies:
+        x = 0
+        for index, row in df.iterrows():
+            x += row['Similarity']*(matrix[row['userId']][movieId]- get_avg(matrix[row['userId']].values()))
+        expected_rate = avg_rating_target + x
+        expected_rates.append(expected_rate)
     
     return recommendations
 
@@ -38,7 +69,7 @@ def user_based_recommender(target_user_idx, matrix):
 if __name__ == "__main__":
     
     # Load the dataset
-    path_to_ml_latest_small = '/home/albert/Projects/practica_recomenders/ml-latest-small/'
+    path_to_ml_latest_small = 'C:/Users/thema/Desktop/proyectos/caim/lab/session9recomenders/ml-latest-small/'
     dataset = ut.load_dataset_from_source(path_to_ml_latest_small)
 
     # Ratings data
